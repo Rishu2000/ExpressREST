0a2283e7630e18362b8a35a778ba4ed62cd177c3
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-20 22:29:34 +0530

    Created a basic Express JS Web Server.

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("object");
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.get("/",(req,res) => {
+    res.json("Hello, GFG!")
+});
+
+app.listen(port,() =>{
+    console.log("server started in "+port);
+});
