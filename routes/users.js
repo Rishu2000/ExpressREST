504490f905232fb30c488dcbd796e8420915dbce
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-21 17:52:56 +0530

    Implemented users and single user and also removed password

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,22 @@ const users = require("../constants/users");
 const app = express.Router();
 
 app.get("/",(req,res) => {
-    res.json("Welcome to users.js");
+    res.json(users.map((user) => {
+        const a = {...user};
+        delete a.Password;
+        return a;
+    }));
+});
+
+app.get("/:id",(req,res) => {
+    const id = +req.params.id;
+    if(users[id]){
+        const a = {...users[id]};
+        delete a.Password;
+        res.json(a);
+    }else{
+        res.status(404).json("Error! User Not Found!");
+    }
 });
 
 module.exports = app;
