6f40d981d49759e9b36ddfbcbfde9d9839569d8b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-21 17:16:07 +0530

    Added some constants.

---
+++ constants/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        Name:"Rishav Kumar Shah",
+        Password:"ris@1234",
+        LinkedIn:"https:Rishav//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
+    },{
+        Name:"Ankit Kumar Shah",
+        Password:"ank@1234",
+        LinkedIn:"https:Ankit//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
+    },{
+        Name:"Praduman Kumar Shah",
+        Password:"prad@1234",
+        LinkedIn:"https:Praduman//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
+    },{
+        Name:"Rishu Kumar",
+        Password:"helloRis@1234",
+        LinkedIn:"https:Rishu//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
+    },{
+        Name:"Kumar Shah",
+        Password:"kum@1234",
+        LinkedIn:"https:Kumar//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
+    },
+]
