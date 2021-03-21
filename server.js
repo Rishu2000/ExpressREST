8f93fd5935cc5cbd41ecda2e7e0b826ff6fe1489
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-21 16:02:48 +0530

    Implemented a modular routing technique.

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const root = require("./routes/root")
 const app = express();
 const port = 80;
 
-app.get("/",(req,res) => {
-    res.json("Hello, GFG!")
-});
+app.use("/",root);
+
+// const todos = ["Milk","Curd","Suger"]
+
+// app.get("/",(req,res) => {
+//     res.json("Welcome to GFGs API Server!")
+// });
+
+// app.get("/api/",(req,res) => {
+//     res.json("This is the root of API server")
+// });
+
+// app.get("/api/todos/:id",(req,res) => {
+//     if(!todos[+req.params.id]){
+//         res.status(404).json("error! Cannot find it!")
+//     }else{
+//         res.json(todos[+req.params.id]);
+//     }
+// })
 
 app.listen(port,() =>{
     console.log("server started in "+port);
 });
-
-// Mapping
-//     (basic)                         (user friendly)
-//     fetch                              Axios
-//     http-server                        express
