a0ad76f6262023032726599d671ba6929bffb200
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-21 17:59:36 +0530

    Implemented morgan-middleware.

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const root = require("./routes/root")
 const app = express();
 const port = 80;
 
+app.use(morgan("dev"));
 app.use("/",root);
-
-// const todos = ["Milk","Curd","Suger"]
-
-// app.get("/",(req,res) => {
-//     res.json("Welcome to GFGs API Server!")
-// });
-
-// app.get("/api/",(req,res) => {
-//     res.json("This is the root of API server")
-// });
-
-// app.get("/api/todos/:id",(req,res) => {
-//     if(!todos[+req.params.id]){
-//         res.status(404).json("error! Cannot find it!")
-//     }else{
-//         res.json(todos[+req.params.id]);
-//     }
-// })
 
 app.listen(port,() =>{
     console.log("server started in "+port);
