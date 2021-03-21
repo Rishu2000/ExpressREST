2bfd57f8273b9c62689aede5d7d2ca24ca9b3b29
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-21 17:31:16 +0530

    Implemented Todo functionality.

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,16 @@ const todos = require("../constants/todos");
 const app = express.Router();
 
 app.get("/",(req,res) => {
-    res.json("Welcome to todos.js");
+    res.json(todos);
+});
+
+app.get("/:id",(req,res) => {
+    const id = +req.params.id;
+    if(todos[id]){
+        res.json(todos[id])
+    }else{
+        res.status(404).json("Error! Todo Not Found!");
+    }
 });
 
 module.exports = app;
