76c3cc9190350f753b4e15418743d1e19bac4b5b
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-22 11:04:52 +0530

    Added a Username field.

--- constants/users.js
+++ constants/users.js
@@ -1,23 +1,28 @@
 module.exports = [
     {
+        Username:"Rishav",
         Name:"Rishav Kumar Shah",
         Password:"ris@1234",
         LinkedIn:"https:Rishav//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
     },{
+        Username:"Ankit",
         Name:"Ankit Kumar Shah",
         Password:"ank@1234",
         LinkedIn:"https:Ankit//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
     },{
+        Username:"Praduman",
         Name:"Praduman Kumar Shah",
         Password:"prad@1234",
         LinkedIn:"https:Praduman//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
     },{
+        Username:"Rishu",
         Name:"Rishu Kumar",
         Password:"helloRis@1234",
         LinkedIn:"https:Rishu//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
     },{
+        Username:"Kumar",
         Name:"Kumar Shah",
         Password:"kum@1234",
         LinkedIn:"https:Kumar//www.linkedin.com/in/rishav-kumar-shah-11a18a179/"
-    },
+    }
 ]
