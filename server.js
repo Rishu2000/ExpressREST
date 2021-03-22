765efa082b3068463622d69d0c96ae6a6310db9f
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-22 19:21:31 +0530

    Implemented session middleware.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const session = require("express-session");
 const root = require("./routes/root")
 const app = express();
 const port = 80;
 
 app.use(express.json());
+app.use(
+    session({
+        secret: "corona"
+    }))
 app.use(morgan("dev"));
 app.use("/",root);
 
