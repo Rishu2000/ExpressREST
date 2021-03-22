18a04016dbd14c926516c94101653e4674c131a0
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-03-22 18:50:54 +0530

    Single client authentication implemented.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 const users = require("../constants/users");
 const app = express.Router();
 
+let Authentication = false;
+
+app.use(express.json());
+
 app.get("/",(req,res) => {
     res.json(users.map((user,key) => {
         const a = {UserID:key,...user};
@@ -9,6 +13,28 @@ app.get("/",(req,res) => {
         return a;
     }));
 });
+
+app.post("/login",(req,res) => {
+    const {Username,Password} = req.body;
+    if(!Username || !Password)
+        res.status(400).json("Require both Username and Password");
+    else{
+        const match = users.filter(u => u.Username.toLowerCase() === Username.toLowerCase() && u.Password === Password)
+        if(match.length === 1){
+            Authentication = true;
+            res.json({Success:true})
+        }else if(match.length === 0){
+            Authentication = false;
+            res.status(401).json("Oops! Bad credentials.")
+        }else{
+            res.status(406).json("multiple user exists.")
+        }
+    }
+})
+
+app.get("/login",(req,res) => {
+    res.json({Authentication});
+})
 
 app.get("/:id",(req,res) => {
     const id = +req.params.id;
